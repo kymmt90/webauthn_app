961e0d546e10b2cc1c550fcf161038390900da75
Kohei Yamamoto <kymmt90@gmail.com>
2020-02-16 22:17:42 +0900

    Define validations of User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  validates :uid, uniqueness: true
+  validates :uid, format: { with: /\A[A-Za-z0-9]{1,32}\z/ }, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'format validations' do
+    assert User.new(uid: 'a').valid?
+    assert User.new(uid: 'a0B').valid?
+    assert User.new(uid: 'a' * 32).valid?
+    assert_not User.new(uid: 'a' * 33).valid?
+  end
+
+  test 'presence validations' do
+    assert_not User.new(uid: '').valid?
+    assert_not User.new(uid: nil).valid?
+  end
 end
