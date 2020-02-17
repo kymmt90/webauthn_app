6afe05561629ecc6fd315bda1529732ce012f044
Kohei Yamamoto <kymmt90@gmail.com>
2020-02-17 22:10:35 +0900

    Define some options

--- db/migrate/20200217130655_create_webauthn_credentials.rb
+++ db/migrate/20200217130655_create_webauthn_credentials.rb
@@ -1,9 +1,9 @@
 class CreateWebauthnCredentials < ActiveRecord::Migration[6.0]
   def change
     create_table :webauthn_credentials do |t|
-      t.text :public_key
-      t.integer :sign_count
-      t.references :setting, null: false, foreign_key: true
+      t.text :public_key, null: false
+      t.integer :sign_count, null: false, default: 0
+      t.references :setting, null: false, foreign_key: { to_table: :webauthn_settings }
 
       t.timestamps
     end
