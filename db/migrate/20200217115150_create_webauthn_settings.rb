4be176e94dcca9ed496d2a3ae28ebdceb6c5986f
Kohei Yamamoto <kymmt90@gmail.com>
2020-02-17 20:53:05 +0900

    webauthn_settings.user_handle must not be null

--- db/migrate/20200217115150_create_webauthn_settings.rb
+++ db/migrate/20200217115150_create_webauthn_settings.rb
@@ -1,11 +1,12 @@
 class CreateWebauthnSettings < ActiveRecord::Migration[6.0]
   def change
     create_table :webauthn_settings do |t|
-      t.string :user_handle
+      t.string :user_handle, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+
     add_index :webauthn_settings, :user_handle, unique: true
   end
 end
